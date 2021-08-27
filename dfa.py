f541b997372acc9851c8019f15b6a5c4d2c23cea
Jose Hernandez <jhernandez.sld@e.email>
2021-08-27 16:35:47 -0500

    modified examples and AFD

--- dfa.py
+++ dfa.py
@@ -4,24 +4,27 @@ from typing import Callable, Iterable, Dict, Set, Tuple, Any
 
 
 def compose_functions(f: Callable, g: Callable) -> Callable:
-    def fog(x:Any) -> Any:
+    def fog(x: Any) -> Any:
         return g(f(x))
+
     return fog
 
-def or_function(v1: bool, v2: bool)-> bool:
+
+def or_function(v1: bool, v2: bool) -> bool:
     return v1 or v2
 
-def deterministic_automate(sigma: Set[chr],
-                           delta: Dict[Tuple[chr,str], str],
-                           f: Set[str],
-                           s: str):
+
+def deterministic_automate(
+    sigma: Set[chr], delta: Dict[Tuple[chr, str], str], f: Set[str], s: str
+):
     def delta_fn(state: str, char: chr) -> str:
         print(f'{state} , {char} : {delta.get((state,char), "_")}')
-        return delta.get((state,char), "_")
+        return delta.get((state, char), "_")
 
-    def partial_delta(char:chr) -> Callable[[str],str]:
-        def delta_eval(state:str) -> str:
+    def partial_delta(char: chr) -> Callable[[str], str]:
+        def delta_eval(state: str) -> str:
             return delta_fn(state, char)
+
         return delta_eval
 
     # d(q,s) -> q_1 => d(q_1, s_1) ::  p(s, d) -> dp  :: dp(q) -> q_1 => dp(q_1) => ...
@@ -29,37 +32,32 @@ def deterministic_automate(sigma: Set[chr],
     def compose_delta_transitions(functions: Iterable[Callable]) -> Callable:
         return reduce(compose_functions, functions)
 
-    def create_delta_transitions(word: str) -> Iterable[Callable[[str],str]]:
-        return (partial_delta(char) for char in word) if len(word) > 0 else (lambda x: x, lambda x : x)
+    def create_delta_transitions(word: str) -> Iterable[Callable[[str], str]]:
+        return map(partial_delta, word) if len(word) > 0 else (lambda x: x, lambda x: x)
 
     def evaluate(word: str) -> bool:
-        return compose_delta_transitions(
-            create_delta_transitions(word))(s) in F
+        return compose_delta_transitions(create_delta_transitions(word))(s) in F
 
-    if reduce(or_function ,(v not in sigma for k,v in delta.keys())):
-        raise Exception('char in delta is not in sigma')
+    if reduce(or_function, (v not in sigma for k, v in delta.keys())):
+        raise Exception("char in delta is not in sigma")
     return evaluate
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    # (b(a|b)*b)|""
+    # a*b)|""
     delta_dict = {
-        ("q0", 'a'): "q1" ,
-        ("q0", 'b'): "q1" ,
-        ("q1", 'a'): "q3" ,
-        ("q1", 'b'): "q3" ,
-        ("q3", 'a'): "q0" ,
-        ("q3", 'b'): "q0"
+        ("q0", "a"): "q0",
+        ("q0", "b"): "q1",
     }
-    sigma = {'a','b'}
-    F = {"q0"}
+    sigma = {"a", "b"}
+    F = {"q1"}
     s = "q0"
     da = deterministic_automate(sigma, delta_dict, F, s)
-    print(da("ba"))
-    print(da("baaaab"))
+    print(da("ab"))
+    print(da("aaaab"))
     print(da("bbbbbbbbbbbbbb"))
     print(da("bbbbbbbbbbbbbbbb"))
-    print(da("bbab"))
+    print(da("aaaaab"))
     print(da(""))
-    print(da("abababab"))
+    print(da("b"))
